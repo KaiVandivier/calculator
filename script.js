34fa14b3386364b11e14f56973d151a1d4059656
Kai Vandivier <kpvandivier@gmail.com>
2019-07-03 21:11:24 -0700

    Add better input command-chaining functionality

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-// Make functions that operator buttons:
-// On each button, have an event listener on click
-// When clicked:
-  // if it's a number, add it to the display value
-  // if it's a decimal, add it to the display value and DISABLE
-  // if it's an operator, store it AND the displayed number as n1; 
-    // next button will clear the display and ENABLE decimal
-    // Note: store number string as an integer
-  // (or, display operator, and continue? more difficult to parse operation)
-  // (if that's the case, it might make sense to store the displayed value separate from actual operations)
-  // if it's an equals, store n2 call operate(operator, n1, n2)
-
 
 function newEquation(eq) {
   eq.n1 = null;
   eq.n2 = null;
   eq.operator = null;
-  eq.decimal = null;
-  return eq;
+  eq.result = null;
 }
 
 function loadButtons() {
@@ -63,23 +50,27 @@ function pressNum(num) {
     newEquation(eq);
     clearDisplay();
   }
-  if (display.textContent === "0") display.textContent = num;
+  if (display.textContent === "0" || display.textContent == eq.n1) {
+    display.textContent = num;
+  } 
   else if (display.textContent.length < 9) display.textContent += num;
-  // TODO: handle screen refresh once operator is pressed
 }
 
 function pressOp(op) {
   const display = document.querySelector('#display');
-  if (eq.n1 && eq.n2) newEquation(eq);
+  if (eq.operator) equals();
+  if (eq.n2) newEquation(eq);
   if (!eq.operator) eq.operator = op;
   if (!eq.n1) eq.n1 = parseFloat(display.textContent);
-  // TO DO WEDNESDAY: use updateScreen() function and eq instead of using textContent
-  clearDisplay(display);
-  // TODO: keep number on screen until next button is pressed
 }
 
 function addDecimal() {
   const display = document.querySelector('#display');
+  if (eq.n1 && eq.n2) {
+    newEquation(eq);
+    clearDisplay();
+  }
+  if (display.textContent == eq.n1) clearDisplay(); 
   if (!display.textContent.includes('.') && display.textContent.length < 9) {
     display.textContent += '.';
   };
@@ -89,9 +80,11 @@ function equals() {
   const display = document.querySelector('#display');
   if (!eq.n2) eq.n2 = parseFloat(display.textContent);
   const result = operate(eq.operator, eq.n1, eq.n2);
-  display.textContent = result.toString().slice(0,9);
+  if (result !== null) display.textContent = 
+      (result.toString().length > 9) ? result.toExponential(3).toString()
+      : result.toString().slice(0,9);
+  // TODO: Make text display smarter (scientific notation, for example)
 }
-
 
 function clearDisplay() {
   const display = document.querySelector('#display');
